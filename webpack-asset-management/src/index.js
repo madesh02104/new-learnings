4e89737a77ef1114c4551cbef63b1a1c62601742
madesh02104 <madesh02104@gmail.com>
2024-08-06 22:45:14 +0530

    removed unwanted comment

--- webpack-asset-management/src/index.js
+++ webpack-asset-management/src/index.js
@@ -1,4 +1,3 @@
-// import your function
 import myName from './myName';
 import './style.css';
 // !!credits!! icon author : https://www.flaticon.com/packs/tourism-16138565
@@ -9,11 +8,9 @@ import Notes from './data.csv';
 function component() {
   const element = document.createElement('div');
 
-  // use your function!
   element.textContent = myName('Madesh');
   element.classList.add('hello');
 
-  // Add the image to our existing div.
   const myIcon = new Image();
   myIcon.src = Icon;
 
