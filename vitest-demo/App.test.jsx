63186f85bbb8429ddd66cc7a0eb1d40d4ddf488f
madesh02104 <madesh02104@gmail.com>
2024-10-09 21:49:25 +0530

    started with routing in react

--- vitest-demo/App.test.jsx
+++ vitest-demo/App.test.jsx
@@ -1,5 +1,3 @@
-// App.test.jsx
-
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
@@ -7,7 +5,6 @@ import App from "./src/App";
 
 describe("App component", () => {
   it("renders magnificent monkeys", () => {
-    // since screen does not have the container property, we'll destructure render to obtain a container for this test
     const { container } = render(<App />);
     expect(container).toMatchSnapshot();
   });
