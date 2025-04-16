c6e4a8be0ba15b410f87f8260fdc4587eb4fea8d
madesh02104 <madesh02104@gmail.com>
2025-04-16 20:14:46 +0530

    learned pagination and search

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -2,7 +2,7 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchCardData } from "@/app/lib/data"; // Remove fetchLatestInvoices
+import { fetchCardData, fetchRevenue } from "@/app/lib/data";
 import { Suspense } from "react";
 import {
   RevenueChartSkeleton,
@@ -12,7 +12,8 @@ import {
 import CardWrapper from "@/app/ui/dashboard/cards";
 
 export default async function Page() {
-  // Remove `const latestInvoices = await fetchLatestInvoices()`
+  // Fetch data in parallel
+  const revenue = await fetchRevenue();
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -32,7 +33,7 @@ export default async function Page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart revenue={[]} />
+          <RevenueChart revenue={revenue} />
         </Suspense>
         <Suspense fallback={<LatestInvoicesSkeleton />}>
           <LatestInvoices />
