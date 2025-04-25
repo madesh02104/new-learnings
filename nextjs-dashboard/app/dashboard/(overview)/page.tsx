9a868c0918227052df2bbf6f9cba753b20be1151
madesh02104 <madesh02104@gmail.com>
2025-04-25 19:58:48 +0530

    learned handling error and auth

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -1,4 +1,3 @@
-import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
@@ -14,12 +13,6 @@ import CardWrapper from "@/app/ui/dashboard/cards";
 export default async function Page() {
   // Fetch data in parallel
   const revenue = await fetchRevenue();
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
 
   return (
     <main>
